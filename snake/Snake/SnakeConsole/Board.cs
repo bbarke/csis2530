433593e23b3504d6c29c4bdf474c2ec7ebe32ba5
Foxhound77 <elderstrickland@yahoo.com>
2014-11-07 18:01:19 -0700

    I made changes to the board.cs and program.cs

--- snake/Snake/SnakeConsole/Board.cs
+++ snake/Snake/SnakeConsole/Board.cs
@@ -8,6 +8,24 @@ namespace SnakeConsole
 {
     class Board
     {
-        // this is awesome
+        
+        private int[,] board;
+
+        public Board(int totalRow, int totalCol)
+        {
+            board = new int[totalRow, totalCol];
+        }
+
+        public void PrintBoard()
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    Console.Write(board[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
--- snake/Snake/SnakeConsole/Program.cs
+++ snake/Snake/SnakeConsole/Program.cs
@@ -12,6 +12,8 @@ namespace SnakeConsole
         {
             // fork a project
             // type: git clone <url of project>
+            Board b1 = new Board(50, 50);
+            b1.PrintBoard();
 
         }
     }
