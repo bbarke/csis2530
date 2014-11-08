e28b0e94e63b5a194df3c3eedd8e878dcc390199
Thuy Nguyen <tiffastic@hotmail.com>
2014-11-07 17:49:26 -0700

    I made more comments without exclamation

--- snake/Snake/SnakeConsole/Program.cs
+++ snake/Snake/SnakeConsole/Program.cs
@@ -13,6 +13,15 @@ namespace SnakeConsole
             // fork a project
             // type: git clone <url of project>
 
+            // To commit and push to git hub:
+            // View --> Team Explorer
+            // click the top tap where you see csis2530(Local)
+            // Select Changes
+            // Expand
+            // git pull upstream master --> type this in to fetch changes
+            // git status
+            // git log
+
         }
     }
 }
